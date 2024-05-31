6a9af50b9c6cbed0341482a3e23559056312b40b
«Ваше <fr33lancemail@gmail.com>
2024-05-31 19:09:44 +0200

    Update sorting resorts on home_page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   # об отелях, отзывы, форму обратной связи и другие общие элементы.
   def index
     page_number = params[:page].presence || 1
-    @resorts = Resort.page(page_number.to_i).per(2)
+    @resorts = Resort.order(created_at: :asc).page(page_number.to_i).per(2)
   end
 end
