2a1b1609bb573e5caab600b62fe75edbb22d3d0f
«Ваше <fr33lancemail@gmail.com>
2024-06-28 16:25:04 +0200

    Ф

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,24 @@ class HomeController < ApplicationController
     page_number = params[:page].presence || 1
     @resorts = Resort.order(created_at: :asc).page(page_number.to_i).per(2)
   end
+
+  def new
+    @resort = Resort.new
+  end
+
+  def create
+    @resort = Resort.new(resort_params)
+    if @resort.save
+      redirect_to @resort
+      flash[:success] = 'Resort was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def resort_params
+    params.require(:resort).permit(:title, :description, :location, images: [])
+  end
 end
